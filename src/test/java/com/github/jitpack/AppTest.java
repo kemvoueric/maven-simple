c2a50ee83a0f5bf23fe992451ae96c0e2afa665b
kemvoueric <75843522+kemvoueric@users.noreply.github.com>
2021-09-08 20:48:43 -0500

    Update AppTest.java

--- src/test/java/com/github/jitpack/AppTest.java
+++ src/test/java/com/github/jitpack/AppTest.java
@@ -18,6 +18,133 @@ public class AppTest
     @Test
     public void testTrue()
     {
-        assertTrue( true );
+        assertTrue( true );}
     }
 }
+package com.github.jitpack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest 
+{
+    App app = new App();
+
+    @Test
+    public void testApp()
+    {
+        assertEquals("Hello world", app.greet("world"));
+    }
+
+    @Test
+    public void testTrue()
+    {
+        assertTrue( true );}
+    package com.github.jitpack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest 
+{
+    App app = new App();
+
+    @Test
+    public void testApp()
+    {
+        assertEquals("Hello world", app.greet("world"));
+    }
+
+    @Test
+    public void testTrue()
+    {
+        assertTrue( true );}
+    package com.github.jitpack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest 
+{
+    App app = new App();
+
+    @Test
+    public void testApp()
+    {
+        assertEquals("Hello world", app.greet("world"));
+    }
+
+    @Test
+    public void testTrue()
+    {
+        assertTrue( true );}
+    package com.github.jitpack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest 
+{
+    App app = new App();
+
+    @Test
+    public void testApp()
+    {
+        assertEquals("Hello world", app.greet("world"));
+    }
+
+    @Test
+    public void testTrue()
+    {
+        assertTrue( true );}
+    package com.github.jitpack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest 
+{
+    App app = new App();
+
+    @Test
+    public void testApp()
+    {
+        assertEquals("Hello world", app.greet("world"));
+    }
+
+    @Test
+    public void testTrue()
+    {
+        assertTrue( true );}
+    package com.github.jitpack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest 
+{
+    App app = new App();
+
+    @Test
+    public void testApp()
+    {
+        assertEquals("Hello world", app.greet("world"));
+    }
+
+    @Test
+    public void testTrue()
+    {
+        assertTrue( true );}
+    
