8e421c1d84224f5b52e7beb51ad6b499fabdfc8b
kemvoueric <75843522+kemvoueric@users.noreply.github.com>
2021-09-08 20:48:06 -0500

    Update App.java

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -15,3 +15,192 @@ public class App
         return "Hello " + name;
     }
 }
+
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}}}vv
+}
+}}
+}
